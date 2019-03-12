9051089f3ceabaf8dcc5d41b26ef2867b0f610e8
Michael Gfeller <mgfeller@hsr.ch>
2019-03-12 07:22:47 +0100

    update

--- Vorlesung/Vorlesung/10_Array/04_array_functions.js
+++ Vorlesung/Vorlesung/10_Array/04_array_functions.js
@@ -2,10 +2,6 @@ let x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log("has negative", x.some(x => x < 0));
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-x.shift();
-console.log("removed first item", x);
-
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log("filter", x.filter(x => x % 2 == 0));
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -23,3 +19,7 @@ x.sort((x, y) => x - y);
 console.log("sorted 2", x);
 x.sort((x, y) => y - x);
 console.log("sorted 3", x);
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+x.shift();
+console.log("removed first item", x);
