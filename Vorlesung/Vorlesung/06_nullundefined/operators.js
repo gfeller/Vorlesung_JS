da2e128b51ed2a9c15978c61a2273dd72d4e7872
Michael Gfeller <michael.gfeller@ost.ch>
2021-01-07 10:48:18 +0100

    updated examples

---
+++ Vorlesung/Vorlesung/06_nullundefined/operators.js
@@ -0,0 +1,9 @@
+const undef = undefined;
+console.log(undef || 1);
+
+const zero = 0;
+console.log(zero || 1); // good or bad?
+
+console.log(zero ?? 1); // solution
+console.log(NaN ?? 1); // solution
+console.log("" ?? 1); // solution
---
+++ Vorlesung/Vorlesung/17_Hash/hash-example.js
@@ -0,0 +1,20 @@
+const items = [
+    {name: "Ananas", price: 3, amount: 5},
+    {name: "Shirt", price: 30, amount: 1},
+    {name: "Auto", price: 3000, amount: 1},
+    {name: "Bread", price: 1.5, amount: 10},
+];
+
+const sortType = {
+    "string": (a, b) => a > b ? 1 : a < b ? -1 : 0,
+    "number": (a, b) => a - b
+}
+
+function sortItemsBy(items, sort) {
+    const sortFn = sortType[typeof (items[0][sort])];
+    return [...items].sort((a, b) => sortFn(a[sort], b[sort]));
+}
+
+console.log(sortItemsBy(items, "price"));
+console.log(sortItemsBy(items, "amount"));
+console.log(sortItemsBy(items, "name"));
