f5576692bc0780648985cae87fe46ed2494f7a7e
Michael Gfeller <michael.gfeller@ost.ch>
2023-05-03 20:39:56 +0200

    changed

--- Uebungen/functions/function1.js
+++ Uebungen/functions/function1.js
@@ -11,9 +11,6 @@ function sayHi(where){
 sayHi((x)=>console.log(x));
 sayHi(console.log);
 sayHi(console.log());
-sayHi((x)=>console.error(x));
-sayHi(console.error);
-sayHi(console.error());
 
 console.fun = function(){
     this.log("haha");
