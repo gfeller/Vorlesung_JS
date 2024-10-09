4296b61d054ab6bff64fb610b4459859c98ad564
Michael Gfeller <michael.gfeller@ost.ch>
2024-10-09 07:37:16 +0200

    small update

--- Vorlesung/Vorlesung/02_boolean/example-usage-2.js
+++ Vorlesung/Vorlesung/02_boolean/example-usage-2.js
@@ -1,18 +1,2 @@
-const readline = require('node:readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-rl.question(`Welchen Service?
-0:Kündigung
-1:Bestellung
-2:Information
-`, selected => {
-    if(Number(selected)){  // was passiert bei einer '0'?
-        console.log("selected", selected);
-    }
-    else{
-        console.log("nothing", selected);
-    }
-    rl.close();
-});
+const theAnswer = parseFloat("abc") || 42;
+console.log(theAnswer)
